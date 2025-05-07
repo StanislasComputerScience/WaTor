72297246b2f2ddb9178bd5daf3ca97c2f413c82d
StanislasComputerScience <remilabonne@yahoo.fr>
2025-05-07 07:59:31 +0200

    j'ai ajouté neibors dans fish et sea

--- wator.py
+++ wator.py
@@ -1,24 +1,39 @@
+class Sea:
+    pass
+
 class Fish:
-        def __init__(self, pregnancy, age, x_coordinate, y_coordinate):
-            self.pregnancy = pregnancy
-            self.age = age
-            self.x_coordinate = x_coordinate
-            self.y_coordinate = y_coordinate
+    def __init__(self, pregnancy, age, x_coordinate, y_coordinate):
+        self.pregnancy = pregnancy
+        self.age = age
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.sea = None
 
-        def move():
-            pass
-        def reproduce():
-            pass
+    def get_neighbors(self):
+        directions = [(-1, 0), (1, 0), (0,-1), (0, 1),(-1,-1),(-1, 1),(1, 1),(1, -1)]
+        neighbors_coord = []
+        for dx, dy in directions:
+            nx, ny = (self.x_coordinate + dx) % self.sea.width, (self.y_coordinate + dy) % self.sea.length
+            neighbors_coord.append((nx, ny))
+        return list(map(lambda t: self.sea.sea[t[0]][t[1]],neighbors_coord))
 
-class Shark:
-        def __init__(self, pregnancy, age, x_coordinate, y_coordinate, energy):
-            super().__init__(pregnancy, age, x_coordinate, y_coordinate)
-            self.energy = energy
-        def eat():
-            pass
 
-#from models.fish import Fish
-#from models.shark import Shark
+    def move(self):
+        # Implémentez la logique de déplacement ici
+        pass
+
+    def reproduce(self):
+        # Implémentez la logique de reproduction ici
+        pass
+
+class Shark(Fish):
+    def __init__(self, pregnancy, age, x_coordinate, y_coordinate, energy):
+        super().__init__(pregnancy, age, x_coordinate, y_coordinate)
+        self.energy = energy
+
+    def eat(self):
+        # Implémentez la logique d'alimentation ici
+        pass
 
 class Sea:
     def __init__(self, width=50, length=30):
@@ -26,14 +41,11 @@ class Sea:
         self.width = width
         self.length = length
 
-    def add_fish(self,fish_obj):
-        """_summary_
-            ajout d'un poisson (requin ou autre)
-        Args:
-            fish_obj (_type_): objet de type poisson
-        """
+    def add_fish(self, fish_obj):
         x = fish_obj.x_coordinate
         y = fish_obj.y_coordinate
+        fish_obj.sea = self
+
         if 0 <= x < len(self.sea) and 0 <= y < len(self.sea[0]):
             if self.sea[x][y] is None:
                 self.sea[x][y] = fish_obj
@@ -42,25 +54,49 @@ class Sea:
         else:
             print(f"Coordonnées ({x},{y}) hors limites.")
 
+
+
     def print_sea(self):
-        """fonction pour imprimer la mer, avec une variable en fonction de si la case est vide ou occupée.
-        """
         for row in self.sea:
             for cell in row:
                 if cell is None:
-                    print('\033[44m🌊\033[0m', end='')  # océan bleu
-                elif isinstance(cell, Fish):
-                    print('\033[43m🐟\033[0m', end='')  # poisson jaune
+                    print('\033[44m🌊\033[0m', end='') # océan bleu
                 elif isinstance(cell, Shark):
-                    print('\033[41m🦈\033[0m', end='')  # requin rouge
+                    print('\033[41m🦈\033[0m', end='') # requin rouge
+                elif isinstance(cell, Fish):
+                    #print('\033[43m🐟\033[0m', end='') # poisson jaune
+                    print('\033[42m🐟\033[0m', end='') # poisson vert
+
                 else:
-                    print(f'{cell} ', end='')  # debug
+                    print(f'{cell} ', end='') # debug
             print()
 
+    def __str__(self):
+        output = ""
+        for row in self.sea:
+            for cell in row:
+                if cell is None:
+                    output += '\033[44m🌊\033[0m'
+                elif isinstance(cell, Shark):
+                    output += '\033[41m🦈\033[0m'
+                elif isinstance(cell, Fish):
+                    output += '\033[43m🐟\033[0m'
+            output += "\n"
+        return output
 
-#from models.sea import Sea
-#my_sea = Sea()
-#my_sea.print_sea()
+    def __repr__(self):
+        return str(self)
 
-f = Fish(5, 0, 1, 1)
-s = Shark(5, 0, 3, 3, 10)
+
+# Test
+my_sea = Sea()
+
+d_fish={'pregnancy' : 3, 'age' : 0, 'x_coordinate' : 1, 'y_coordinate' : 1}
+d_shark={'pregnancy' : 5, 'age' : 0, 'x_coordinate' : 2, 'y_coordinate' : 2, 'energy' : 3}
+
+f = Fish(**d_fish) 
+s = Shark(**d_shark)
+my_sea.add_fish(f)
+my_sea.add_fish(s)
+print(my_sea)
+print(f.get_neighbors())
